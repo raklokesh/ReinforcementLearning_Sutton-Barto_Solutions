30908fdc8d6ccffec42d9a4c1e0f8b41c49aabb3
Rakshith6 <rakushlok@gmail.com>
2018-10-02 14:36:09 -0400

    Update k_ArmBandit.py

--- Bandit Problem/k_ArmBandit.py
+++ Bandit Problem/k_ArmBandit.py
@@ -40,69 +40,68 @@ class agent:
                 self.UCB_action_value == self.UCB_action_value.max()))  # choosing an action from actions having max of upper confidence bound adjusted action value
             return action
 
-if __name__ == '__main__':
 
-    StdDev=1.0
-    NTrials=1000# no of trials in each experiment
-    NExperiments=1000
-    action_history = np.zeros(NTrials)
+StdDev=1.0
+NTrials=1000# no of trials in each experiment
+NExperiments=1000
+action_history = np.zeros(NTrials)
+reward_history = np.zeros(NTrials)
+reward_accumulation=np.zeros(NTrials)
+action_proportion_history_all=np.zeros((NTrials,len(bandit_reward)))
+reward_history_all=np.zeros(NTrials)
+reward_accumulation_all=np.zeros(NTrials)
+method = 'epsilon' # select epsilon or UCB method for choosing action
+
+for experiment in range(NExperiments):
+    print("Running experiment number {}".format(experiment))
+    action_proportion_history = []
     reward_history = np.zeros(NTrials)
-    reward_accumulation=np.zeros(NTrials)
-    action_proportion_history_all=np.zeros((NTrials,len(bandit_reward)))
-    reward_history_all=np.zeros(NTrials)
-    reward_accumulation_all=np.zeros(NTrials)
-    method = 'epsilon' # select epsilon or UCB method for choosing action
+    Player = agent(epsilon)
+    for trial in range(NTrials):
+        action=Player.choose_action(trial)
+        reward=determine_reward(action)
+        action_history[trial]=action
+        reward_history[trial]=reward
+        reward_accumulation[trial] = np.sum(reward_history)
+        Player.update_value(reward,action)
+        action_proportion_history.append(Player.action_chosen/(trial+1))
 
-    for experiment in range(NExperiments):
-        print("Running experiment number {}".format(experiment))
-        action_proportion_history = []
-        reward_history = np.zeros(NTrials)
-        Player = agent(epsilon)
-        for trial in range(NTrials):
-            action=Player.choose_action(trial)
-            reward=determine_reward(action)
-            action_history[trial]=action
-            reward_history[trial]=reward
-            reward_accumulation[trial] = np.sum(reward_history)
-            Player.update_value(reward,action)
-            action_proportion_history.append(Player.action_chosen/(trial+1))
+        #print("the action taken is {} and the reward is {}".format(action,reward))
 
-            #print("the action taken is {} and the reward is {}".format(action,reward))
+    reward_accumulation_all+=reward_accumulation
+    reward_history_all+=reward_history
+    action_proportion_history_all+=action_proportion_history
 
-        reward_accumulation_all+=reward_accumulation
-        reward_history_all+=reward_history
-        action_proportion_history_all+=action_proportion_history
+action_prop_hist_avg = action_proportion_history_all / NExperiments
+reward_hist_avg = reward_history_all/NExperiments
+reward_accumulation_avg=reward_accumulation_all/NExperiments
 
-    action_prop_hist_avg = action_proportion_history_all / NExperiments
-    reward_hist_avg = reward_history_all/NExperiments
-    reward_accumulation_avg=reward_accumulation_all/NExperiments
+for i in range(len(bandit_reward)):
+    plt1 =plt.subplot(311)
+    plt1.plot(action_prop_hist_avg[:,i],label='Bandit %s' % (i+1))
 
-    for i in range(len(bandit_reward)):
-        plt1 =plt.subplot(311)
-        plt1.plot(action_prop_hist_avg[:,i],label='Bandit %s' % (i+1))
+plt2 = plt.subplot(312)
+plt2.plot(reward_hist_avg, label='Average reward')
 
-    plt2 = plt.subplot(312)
-    plt2.plot(reward_hist_avg, label='Average reward')
+plt3 = plt.subplot(313)
+plt3.plot(reward_accumulation_avg, label='Accum reward')
 
-    plt3 = plt.subplot(313)
-    plt3.plot(reward_accumulation_avg, label='Accum reward')
+plt1.set_ylim([0,1])
+plt1.set_xlabel("Trials")
+plt1.set_ylabel("Average proportion of picked actions")
+plt1.legend()
+plt2.set_ylim([0, 2])
+plt2.set_xlabel("Trials")
+plt2.set_ylabel("Average reward")
+plt3.set_ylim([0, 1500])
+plt3.set_xlabel("Trials")
+plt3.set_ylabel("Accumulated reward")
 
-    plt1.set_ylim([0,1])
-    plt1.set_xlabel("Trials")
-    plt1.set_ylabel("Average proportion of picked actions")
-    plt1.legend()
-    plt2.set_ylim([0, 2])
-    plt2.set_xlabel("Trials")
-    plt2.set_ylabel("Average reward")
-    plt3.set_ylim([0, 1500])
-    plt3.set_xlabel("Trials")
-    plt3.set_ylabel("Accumulated reward")
+fig=plt.gcf()
+fig.set_size_inches(15,10)
+plt.show()
 
-    fig=plt.gcf()
-    fig.set_size_inches(15,10)
-    plt.show()
-
-    if method == 'epsilon':
-        plt.savefig("Action_Proportion_epsilon={}".format(epsilon)+".png", bbox_inches="tight")
-    else:
-        plt.savefig("Action_Proportion_UCB_c={}".format(c) + ".png", bbox_inches="tight")
+if method == 'epsilon':
+    plt.savefig("Action_Proportion_epsilon={}".format(epsilon)+".png", bbox_inches="tight")
+else:
+    plt.savefig("Action_Proportion_UCB_c={}".format(c) + ".png", bbox_inches="tight")
